359cf722c5c69ffdbc9a95fae2dcb96cad6b7f56
bannzai <kingkong999yhirose@gmail.com>
2018-08-22 20:43:53 +0900

    :sparkles: Try enumeratation exception handler

--- iOSDC-BlackMagick/ViewController.m
+++ iOSDC-BlackMagick/ViewController.m
@@ -15,18 +15,38 @@
 
 @implementation ViewController
 
+void function(NSMutableArray *array) {
+    NSLog(@"%@", array);
+}
+
+void objc_enumerationMutation(id obj) {
+    NSLog(@"%@", obj);
+}
+
+void objc_setEnumerationMutationHandler(void (* _Nullable handler)(id  _Nonnull __strong args)) {
+    NSLog(@"a");
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
+
     Class aClass = objc_allocateClassPair([NSObject class], "A", sizeof([NSObject new]));
     objc_registerClassPair(aClass);
-    
     
     id aaaa = [[NSClassFromString(@"A") alloc] init];
     NSString *string = @"hogehoge";
     
     NSLog(@"A: %@, string: %@", aaaa, string);
+    
+    NSNumber *target = @2;
+    NSMutableArray *array = @[@1, target, @3].mutableCopy;
+    
+    objc_setEnumerationMutationHandler(function);
+    protocol_conformsToProtocol(<#Protocol * _Nullable proto#>, <#Protocol * _Nullable other#>)
+
+    for (NSNumber *number in array) {
+        [array insertObject:@5 atIndex:0];
+    }
 }
 
 @end
